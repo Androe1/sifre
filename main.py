c01934bb333005ee0ec9064dccf113ad71bbc171
Yusuf Vedat <144049733+Androe1@users.noreply.github.com>
2023-09-11 20:21:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import random
 x = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 
 sifre_uz = int(input("Şifre kaç harfli olsun?"))
+sifre_adet = 10
 
-pasw = ""
-
-for i in range(sifre_uz):
-    pasw += random.choice(x)
-
-print(pasw)
+for _ in range(sifre_adet):
+    pasw = ""
+    for i in range(sifre_uz):
+        harf = random.choice(x)
+        while i > 0 and harf == pasw[i - 1]:
+            harf = random.choice(x)
+        pasw += harf
+    print(pasw)
